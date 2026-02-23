f89d0680186d2e729f8210b6d7ba8eaa22916926
Zoe Porta <zoeportagarcia@gmail.com>
2026-02-23 11:30:28 +0100

    refactor(domain): use GetWishlistMapper in CreateWishlistUseCase

---
+++ packages/domain/src/use-cases/create-wishlist.use-case.ts
@@ -0,0 +1,33 @@
+import { Wishlist } from "../aggregates/wishlist";
+import { WishlistRepository } from "../repositories/wishlist.repository";
+import { CreateWishlistInput, WishlistOutput } from "./dtos";
+import { GetWishlistMapper } from "./mappers/get-wishlist.mapper";
+import { randomUUID } from "crypto";
+
+/**
+ * Use case for creating a new wishlist.
+ */
+export class CreateWishlistUseCase {
+  constructor(private readonly wishlistRepository: WishlistRepository) {}
+
+  /**
+   * Executes the use case to create and save a new wishlist.
+   *
+   * @param input - The data for creating the wishlist.
+   * @returns A Promise that resolves to the created WishlistOutput DTO.
+   */
+  async execute(input: CreateWishlistInput): Promise<WishlistOutput> {
+    const wishlist = Wishlist.create({
+      id: randomUUID(),
+      ownerId: input.ownerId,
+      title: input.title,
+      description: input.description,
+      visibility: input.visibility,
+      participation: input.participation,
+    });
+
+    await this.wishlistRepository.save(wishlist);
+
+    return GetWishlistMapper.toOutput(wishlist);
+  }
+}
